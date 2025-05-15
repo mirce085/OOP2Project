9265a00635a6cdd77c569d9eb37511caa3031508
Mirjalal Seyidli <mircalalseyidli2005@gmail.com>
2025-05-15 18:25:00 +0400

    first

--- src/ui/MainApp.java
+++ src/ui/MainApp.java
@@ -1,18 +1,32 @@
 package ui;
 
+import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/resources/views/MainView.fxml"));
+        // Original loading call:
+        // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        // Updated: locate the FXML via the ClassLoader
+        URL fxmlLocation = MainApp.class.getClassLoader().getResource("views/MainView.fxml");
+        if (fxmlLocation == null) {
+            throw new IllegalStateException(
+                    "Cannot find FXML resource 'views/MainView.fxml' on the classpath"
+            );
+        }
+
+        Parent root = FXMLLoader.load(fxmlLocation);
         primaryStage.setTitle("Library Management System");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
